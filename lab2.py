cf0c191c7b9dd5971d8f11d84c0b73b180e69816
adina <adinurb6@gmail.com>
2025-01-23 19:13:23 +0500

    first commit

---
+++ lab2.py
@@ -0,0 +1,54 @@
+#Python Booleans
+'''The following will return False:'''
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
+
+
+#Python Operators
+print(100 + 5 * 3) #the result is -> 115
+
+#Python Lists
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist)) #the result is -> 3
+
+#Access List Items
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])#the result is -> ['cherry', 'orange', 'kiwi']
+
+#Change List Items
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)#the result is -> ['apple', 'blackcurrant', 'watermelon', 'orange', 'kiwi', 'mango']
+
+#Add List Items
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print(thislist)#the result is -> ['apple', 'orange', 'banana', 'cherry']
+
+#Remove List Items
+
+
+#loop Lists
+
+
+#List Comprehension
+
+
+#Sort Lists
+
+
+#Copy Lists
+
+
+#Join Lists
+
+
+#List Methods
+
+
+#List Exercises
